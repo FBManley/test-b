5d9a6597174d42570526661becedcf0175db8d72
jakemanley <jbmanley45@gmail.com>
2023-06-14 21:09:37 -0700

    config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import JakeResume from './public/JakeResume.pdf'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <a href={JakeResume}></a>
       </header>
     </div>
   );
